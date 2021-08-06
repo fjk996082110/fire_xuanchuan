a1e179c6a31503efacad539be2f065cc5945bd53
fjk <fengjiankai@bmsfot.com.cn>
2021-08-06 15:34:31 +0800

    1

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,12 +1,46 @@
 <template>
   <div class="home-wrap">
     this is home
+    <button @click="btn">展示组件</button>
+    <Calendar
+      v-if="showPop"
+      v-model="dateSelect"
+      type="range"
+      color="#3975C6"
+      :show-title="false"
+      :allow-same-day="true"
+      :round="true"
+      :min-date="minDate"
+      :max-date="maxDate"
+      @confirm="onDateConfirm"
+    />
   </div>
 </template>
 
 <script>
+import { Calendar } from 'vant';
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    Calendar,
+  },
+  data() {
+    return {
+      showPop: false,
+      dateSelect: '',
+      minDate: new Date(2021, 0, 1),
+      maxDate: new Date(),
+    }
+  },
+  methods: {
+    btn() {
+      this.showPop = true;
+    },
+    onDateConfirm() {
+      console.log('onDateConfirm', this.dateSelect);
+    }
+  }
 }
 </script>
 
